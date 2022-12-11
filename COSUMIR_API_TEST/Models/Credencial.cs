dc878bc5d407f001bc55260e82232e6a931c25cb
Matias_Boiero <maty_boiero@hotmail.com>
2022-12-10 21:17:55 -0300

    Firt commit

---
+++ COSUMIR_API_TEST/Models/Credencial.cs
@@ -0,0 +1,8 @@
+﻿namespace COSUMIR_API_TEST.Models
+{
+    public class Credencial
+    {
+        public string Email { get; set; }
+        public string Contraseña { get; set; }
+    }
+}
---
+++ COSUMIR_API_TEST/Servicios/Servicio_API.cs
@@ -0,0 +1,160 @@
+﻿using COSUMIR_API_TEST.Models;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace COSUMIR_API_TEST.Servicios
+{
+    public class Servicio_API : IServicio_API
+    {
+        private static string _email;
+        private static string _clave;
+        private static string _baseUrl;
+        private static string _token;
+
+        public Servicio_API()
+        {
+
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+
+            _email = builder.GetSection("ApiSetting:email").Value;
+            _clave = builder.GetSection("ApiSetting:clave").Value;
+            _baseUrl = builder.GetSection("ApiSetting:baseUrl").Value;
+            //_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiJtYXR5X2JvaWVyb0Bob3RtYWlsLmNvbSIsIm5iZiI6MTY3MDcwMTc0OCwiZXhwIjoxNjcwNzg4MTQ4LCJpYXQiOjE2NzA3MDE3NDh9.mQDbLaDu0gbcIKQVqn7d-Ro8as2Rh4ide2Wt1CodVoM";
+        }
+
+        //USAR REFERENCIAS 
+        public async Task Autenticar()
+        {
+
+
+            var cliente = new HttpClient();
+            cliente.BaseAddress = new Uri(_baseUrl);
+
+            var credenciales = new Credencial() { Email = _email, Contraseña = _clave };
+
+
+            var content = new StringContent(JsonConvert.SerializeObject(credenciales), Encoding.UTF8, "application/json");
+            var response = await cliente.PostAsync("api/Auth/login", content);
+            var json_respuesta = await response.Content.ReadAsStringAsync();
+
+            var resultado = JsonConvert.DeserializeObject<ResultadoCredencial>(json_respuesta);
+            _token = resultado.token;
+        }
+
+        public async Task<List<Producto>> Lista()
+        {
+            List<Producto> lista = new List<Producto>();
+
+            await Autenticar();
+
+
+            var cliente = new HttpClient();
+            cliente.BaseAddress = new Uri(_baseUrl);
+            cliente.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _token);
+            var response = await cliente.GetAsync("/api/Productos");
+
+            if (response.IsSuccessStatusCode)
+            {
+
+                var json_respuesta = await response.Content.ReadAsStringAsync();
+                var resultado = JsonConvert.DeserializeObject<List<Producto>>(json_respuesta);
+                lista = resultado;
+            }
+
+            return lista;
+        }
+
+        public async Task<Producto> Obtener(int idProducto)
+        {
+            Producto objeto = new Producto();
+
+            await Autenticar();
+
+
+            var cliente = new HttpClient();
+            cliente.BaseAddress = new Uri(_baseUrl);
+            cliente.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _token);
+            var response = await cliente.GetAsync($"/api/Productos/{idProducto}");
+
+            if (response.IsSuccessStatusCode)
+            {
+
+                var json_respuesta = await response.Content.ReadAsStringAsync();
+                var resultado = JsonConvert.DeserializeObject<Producto>(json_respuesta);
+                objeto = resultado;
+            }
+
+            return objeto;
+        }
+
+        public async Task<bool> Guardar(Producto objeto)
+        {
+            bool respuesta = false;
+
+            await Autenticar();
+
+
+            var cliente = new HttpClient();
+            cliente.BaseAddress = new Uri(_baseUrl);
+            cliente.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _token);
+
+            var content = new StringContent(JsonConvert.SerializeObject(objeto), Encoding.UTF8, "application/json");
+
+            var response = await cliente.PostAsync("/api/Productos", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                respuesta = true;
+            }
+
+            return respuesta;
+        }
+
+        public async Task<bool> Editar(int idProducto, Producto objeto)
+        {
+            bool respuesta = false;
+
+            await Autenticar();
+
+
+            var cliente = new HttpClient();
+            cliente.BaseAddress = new Uri(_baseUrl);
+            cliente.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _token);
+
+            var content = new StringContent(JsonConvert.SerializeObject(objeto), Encoding.UTF8, "application/json");
+
+            var response = await cliente.PutAsync($"/api/Productos/{idProducto}", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                respuesta = true;
+            }
+
+            return respuesta;
+        }
+
+        public async Task<bool> Eliminar(int idProducto)
+        {
+            bool respuesta = false;
+
+            await Autenticar();
+
+
+            var cliente = new HttpClient();
+            cliente.BaseAddress = new Uri(_baseUrl);
+            cliente.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _token);
+
+
+            var response = await cliente.DeleteAsync($"/api/Productos/{idProducto}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                respuesta = true;
+            }
+
+            return respuesta;
+        }
+
+    }
+}
