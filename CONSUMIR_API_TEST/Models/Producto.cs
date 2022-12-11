7d60e0faecedd74547d9b82bbe5c7d221dbea821
Matias_Boiero <maty_boiero@hotmail.com>
2022-12-11 12:51:53 -0300

    actualizo servicios

---
+++ CONSUMIR_API_TEST/Models/Producto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COSUMIR_API_TEST.Models
+{
+    public class Producto
+    {
+
+        public int Id { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime FechaAlta { get; set; }
+
+        [Required]
+        public decimal Precio { get; set; }
+        public bool Activo { get; set; }
+    }
+}
